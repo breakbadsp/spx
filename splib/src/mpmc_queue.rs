d62a4a3dfd1e4575c9402ee727fc05d3da2fea73
Sachin Pandhare <sachinadcet@gmail.com>
2024-03-05 17:01:18 +0530

    splib::mpmc_queue added

--- splib/src/mpmc_queue.rs
+++ splib/src/mpmc_queue.rs
@@ -1,9 +1,38 @@
-mod mpmc_queue;
+use std::collections::VecDeque;
+use std::sync::{Condvar, Mutex};
+pub struct MpmcQueue<T> {
+    inner_: Mutex<VecDeque<T>>,
+    cv_: Condvar,
+}
+
+impl<T> MpmcQueue<T> {
+    pub fn new() -> Self {
+        MpmcQueue {
+            inner_: Mutex::new(VecDeque::<T>::new()),
+            cv_: Condvar::new(),
+        }
+    }
+
+    pub fn enqueue(&self, p_data: T) {
+        let mut inner = self.inner_.lock().unwrap();
+        inner.push_back(p_data);
+        self.cv_.notify_one();
+    }
+
+    pub fn dequeue(&self) -> Option<T> {
+        let mut inner = self.inner_.lock().unwrap();
+        while inner.is_empty() {
+            inner = self.cv_.wait(inner).unwrap();
+        }
+        inner.pop_front()
+    }
+}
 
 #[cfg(test)]
 mod tests {
+    use self::MpmcQueue;
+    use super::*;
     use std::sync::Arc;
-    use crate::mpmc_queue::MpmcQueue;
 
     #[test]
     fn basic_test() {
@@ -26,17 +55,15 @@ mod tests {
 
         let consumer = std::thread::spawn({
             let queue = Arc::clone(&queue);
-            move || {
-                loop {
-                    if let Some(value) = queue.dequeue() {
-                        println!("Popped {}", value);
-                    } else {
-                        break;
-                    }
+            move || loop {
+                if let Some(value) = queue.dequeue() {
+                    println!("Popped {}", value);
+                } else {
+                    break;
                 }
             }
         });
-        
+
         producer.join().unwrap();
         consumer.join().unwrap();
     }
